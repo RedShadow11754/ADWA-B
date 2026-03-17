0108722d6b5852e62b0cd2c0403b5467e75be89b
Abel Alemu <abelalex530@example.com>
2026-03-17 16:47:01 +0300

    Fix folder structure for Render deployment

--- Project_Adwa/adwa_app/apps.py
+++
@@ -1,5 +0,0 @@
-from django.apps import AppConfig
-
-
-class AdwaAppConfig(AppConfig):
-    name = 'adwa_app'
--- Project_Adwa/adwa_app/urls.py
+++
@@ -1,7 +0,0 @@
-from django.urls import path
-from . import views
-
-urlpatterns = [
-    path('response/', views.answer, name='answer'),
-    path('feedback/', views.feedback, name='feedback'),
-]
